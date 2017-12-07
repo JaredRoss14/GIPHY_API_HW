b288932c64375df8f129822ae1af1d369c44ed0f
Jared Ross <JaredARoss14@gmail.com>
2017-12-07 07:26:33 -0500

    new commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
     	// This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
     	var a = $("<button>");
     	// Adding a class
-    	a.addClass("animal");
+    	a.addClass("animal btn-primary");
     	// Adding a data-attribute with a value of the movie at index i
     	a.attr("data-name", animals[i]);
     	// Providing the button's text with a value of the movie at index i
@@ -79,6 +79,63 @@ $(document).ready(function(){
 
 
 
+
+
+
+
+
+
+
+
+    $("button").on("click", function() {
+      // In this case, the "this" keyword refers to the button that was clicked
+      var animal = $(this).attr("data-name");
+
+      // Constructing a URL to search Giphy for the name of the person who said the quote
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        animal + "&api_key=w5jW58lIwZV8xLAzlT1P6YZ0DfyIYhX8&limit=10";
+
+      // Performing our AJAX GET request
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+        // After the data comes back from the API
+        .done(function(response) {
+          // Storing an array of results in the results variable
+          var results = response.data;
+
+          // Looping over every result item
+          for (var i = 0; i < results.length; i++) {
+
+            // Only taking action if the photo has an appropriate rating
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+              // Creating a div with the class "item"
+              var gifDiv = $("<div class='item'>");
+
+              // Storing the result item's rating
+              var rating = results[i].rating;
+
+              // Creating a paragraph tag with the result item's rating
+              var p = $("<p>").text("Rating: " + rating);
+
+              // Creating an image tag
+              var personImage = $("<img>");
+
+              // Giving the image tag an src attribute of a proprty pulled off the
+              // result item
+              personImage.attr("src", results[i].images.fixed_height.url);
+
+              // Appending the paragraph and personImage we created to the "gifDiv" div we created
+              gifDiv.append(p);
+              gifDiv.append(personImage);
+
+              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+              $("#giphy-here").prepend(gifDiv);
+            }
+          }
+        });
+    });
 
 
 
